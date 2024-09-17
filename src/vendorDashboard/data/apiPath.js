bf270f00c4ed3adf1da58dd61937d09194266688
raghavaris018 <raghavaris018@gmail.com>
2024-09-17 23:51:57 +0530

    Update apiPath.js

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
-//export const API_URL = "http://localhost:4000"
+export const API_URL = "http://localhost:4000"
 
-export const API_URL = "https://backend-suby-hw0r.onrender.com/"
+//export const API_URL = "https://backend-suby-hw0r.onrender.com/"
