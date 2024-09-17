37f2a4c7887acf3097f1ff857e28578431959cd4
raghavaris018 <raghavaris018@gmail.com>
2024-09-17 18:24:44 +0530

    React Suby Dashboard

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LandingPage from './vendorDashboard/pages/LandingPage'
+
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      
+      <LandingPage/>
+    </div>
+  )
+}
+
+export default App
+
